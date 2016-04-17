6edc2056c0272286795dbff3139bc518d1a43602
Merciba Team <team@merciba.com>
2016-04-17 23:37:15 +0000

    Added color output

--- index.js
+++ index.js
@@ -5,11 +5,27 @@ var asciiPixels = require('ascii-pixels')
 var spawn = require('child_process').spawn
 var chalk = require('chalk')
 var colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray']
+var colorizers = {};
+
+colors.map(function (color) {
+	colorizers[color] = function (match) {
+		return chalk[color](match)
+	}
+})
+
+var paint = function (frame) {
+// Add custom colors for characters here
+//	frame = frame.replace(/\./g, colorizers.cyan)
+//	frame = frame.replace(/,/g, colorizers.green)
+//	frame = frame.replace(/;/g, colorizers.yellow)
+//	frame = frame.replace(/:/g, colorizers.red)
+	return frame
+}
 
 var getFrame = function () {
 	fs.readFile(path.join(__dirname, 'mirror.jpg'), function (err, buffer) {
 		var frame = asciiPixels(jpeg.decode(buffer))
-		console.log('\033c', frame)
+		console.log('\033c', paint(frame))
 	})
 }
 
@@ -18,13 +34,14 @@ var cycle = function () {
 	var getSnapshot = spawn('/opt/vc/bin/raspistill', [
 		'--output', 
 		'/home/pi/ascii-mirror/mirror.jpg', 
+		'--quality', '50',
 		'--width', '225', 
 		'--height', '115', 
 		'--rotation',  '270', 
 		'--nopreview', 
 		'--timeout', '1', 
 		'--imxfx', 'negative', 
-		'--brightness', '72', 
+		'--brightness', '75', 
 		'--contrast', '30',
 		'--hflip'
 	])
